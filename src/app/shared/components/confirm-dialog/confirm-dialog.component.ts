ba98bff0ef4df6ffa011688b4b59deb1413b55e1
gcwiley <gcwiley@gmail.com>
2024-12-27 00:45:11 +0000

    additional code added

---
+++ src/app/shared/components/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,23 @@
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+
+export interface ConfirmDialogData {
+   title: string;
+   content: string;
+}
+
+@Component({
+   selector: 'app-confirm-dialog',
+   standalone: true,
+   templateUrl: './confirm-dialog.component.html',
+   styleUrl: './confirm-dialog.component.scss',
+   changeDetection: ChangeDetectionStrategy.OnPush,
+   imports: [MatDialogModule, MatButtonModule, MatDialogModule],
+})
+export class ConfirmDialogComponent {
+   constructor(
+      @Inject(MAT_DIALOG_DATA) public data: ConfirmDialogData,
+      public dialogRef: MatDialogRef<ConfirmDialogComponent>
+   ) {}
+}
