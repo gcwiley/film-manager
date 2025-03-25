046aa2a45510d731414edc6c4a69179a85d35891
gcwiley <gcwiley@gmail.com>
2025-03-25 14:02:06 +0000

    additional code added

--- src/app/pages/signin-page/signin-page.component.ts
+++ src/app/pages/signin-page/signin-page.component.ts
@@ -1,5 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FormsModule, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+   FormsModule,
+   FormBuilder,
+   Validators,
+   ReactiveFormsModule,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 // import the angular material modules
@@ -9,6 +14,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 // import the shared components
 import { FooterComponent } from '../../components';
@@ -35,7 +41,12 @@ import { AuthService } from '../../services/auth.service';
    ],
 })
 export class SigninPageComponent {
-   constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {}
+   constructor(
+      private formBuilder: FormBuilder,
+      private authService: AuthService,
+      private router: Router,
+      private snackbar: MatSnackBar
+   ) {}
 
    // create the signin form with email and password fields
    public signinForm = this.formBuilder.group({
@@ -43,12 +54,28 @@ export class SigninPageComponent {
       password: ['', Validators.required],
    });
 
+   // sign in with email and password, if successfull, navigate authenticated user to the home page
    public onSubmitSignIn(): void {
+      // if the form has validation errors, it returns early without doing anything
+      if (this.signinForm.invalid) {
+         return;
+      }
+
       this.authService
-         .signInWithEmailAndPassword(this.signinForm.value.email!, this.signinForm.value.password!)
-         .subscribe(() => {
-            // redirects user to homepage
-            this.router.navigateByUrl('/');
+         .signInWithEmailAndPassword(
+            this.signinForm.value.email!,
+            this.signinForm.value.password!
+         )
+         .subscribe({
+            next: () => {
+               // navigates user to homepage
+               this.router.navigateByUrl('/');
+            },
+            error: () => {
+               this.snackbar.open('Unable to sign in', 'CLOSE', {
+                  duration: 3000,
+               });
+            },
          });
    }
 }
--- src/app/pages/signup-page/signup-page.component.ts
+++ src/app/pages/signup-page/signup-page.component.ts
@@ -9,6 +9,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 // import the shared components
 import { FooterComponent } from '../../components';
@@ -34,7 +35,12 @@ import { AuthService } from '../../services/auth.service';
    ],
 })
 export class SignupPageComponent {
-   constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {}
+   constructor(
+      private formBuilder: FormBuilder,
+      private authService: AuthService,
+      private router: Router,
+      private snackbar: MatSnackBar
+   ) {}
 
    // create the signup with email and password fields
    public signUpForm = this.formBuilder.group({
@@ -42,12 +48,28 @@ export class SignupPageComponent {
       password: [null, [Validators.required]],
    });
 
+   // sign up a new user with email and password, if successfull, navigate new user to the home page
    public onSubmitSignUp() {
+      // if the form has validation errors, it returns early without doing anything
+      if (this.signUpForm.invalid) {
+         return;
+      }
+
       this.authService
-         .createUserWithEmailAndPassword(this.signUpForm.value.email!, this.signUpForm.value.password!)
-         .subscribe(() => {
-            // redirects user to homepage
-            this.router.navigateByUrl('/');
+         .createUserWithEmailAndPassword(
+            this.signUpForm.value.email!,
+            this.signUpForm.value.password!
+         )
+         .subscribe({
+            next: () => {
+               // navigates user to homepage
+               this.router.navigateByUrl('/');
+            },
+            error: () => {
+               this.snackbar.open('Unable to create new account', 'CLOSE', {
+                  duration: 3000,
+               });
+            },
          });
    }
 }
